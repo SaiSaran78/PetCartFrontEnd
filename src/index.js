49ade454a0ce90b5d2ead60cea92dafd6887013b
SaiSaran78 <saisaran78@gmail.com>
2023-10-04 08:41:23 +0530

    first message

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import Login from './components/Pages/Login';
-// import OTP from './components/Pages/OTP';
 import App from './App';
-// import Home from './components/Pages/home';
-// import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
